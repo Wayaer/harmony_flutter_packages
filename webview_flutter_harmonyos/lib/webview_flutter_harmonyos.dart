f050d34efb0fbe456800f7cd4126d57d66e81236
Wayaer <Wayaer>
2025-08-26 01:05:54 +0800

    添加 webview_flutter_harmonyos 但有问题

---
+++ webview_flutter_harmonyos/lib/webview_flutter_harmonyos.dart
@@ -0,0 +1,5 @@
+export 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
+
+export 'src/ohos_webview_controller.dart';
+export 'src/ohos_webview_cookie_manager.dart';
+export 'src/ohos_webview_platform.dart';
